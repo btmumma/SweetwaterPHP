855053008dea21521a897fe6b48c774ae02e0873
Benjamin True Mumma <benmumma73@gmail.com>
2022-08-10 20:01:09 -0400

    Add Comments in DBH.INC.PHP

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,13 @@
 <?php
-
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "sweetwatertable";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+// Connection string class.
+// dbh.inc.php = Database Helper Include 
+// Storing the connection string and mysqli connection in here so it doesn't bloat up index
+// mysqli_connect connects to mysql db sweetwatertable stored on http://localhost/phpmyadmin/index.php?route=/database/structure&db=sweetwatertable
+// created table sweetwater_test with given SQL command 
