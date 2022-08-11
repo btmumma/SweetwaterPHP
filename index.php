3a7d9b34e55883e7d14f653d085da724823d25d3
Benjamin True Mumma <benmumma73@gmail.com>
2022-08-10 20:27:05 -0400

    Update Home Page To Create Table
    Commit code to a table to create PHP Table

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
     include_once 'includes/dbh.inc.php';
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
 <head>
 <body>
 
@@ -16,12 +19,25 @@
 
     if($resultCheck > 0)
     {
+        // Create table header
+        echo "<table><tr><th>Order ID</th><th>Comments</th><th>Expected Ship Date</th></tr>";
+        
+        // Iterate over all of the rows we got back from the database...
         while($row = mysqli_fetch_assoc($result))
         {
-            echo $row['orderid'];
-            echo $row['comments'];
-            echo $row['shipdate_expected'];
+            // Create the table data objects dynamically with the data we got back.
+            echo "<tr><td>".$row["orderid"]."</td>  <td>".$row["comments"]."</td> <td>".$row["shipdate_expected"]."</td> </tr>";
+
+
+            // Test to see if I can access column headers:
+            //echo $row['orderid'] . "<br>";
+            //echo $row['comments'] . "<br>";; 
+            //echo $row['shipdate_expected'] . "<br>";; 
         }
+    }
+    else
+    {
+        echo "Ben is an idiot and can't connect";
     }
 ?>
 
